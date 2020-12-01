a0edcf8ba6edd3ad870a400dd1e57ff8e04f04e3
Adminfoco <Adminfoco@10.0.0.24>
2020-11-30 20:21:11 -0500

    hoy

--- src/main/java/edu/cibertec/capitulo4/service/MatriculaService.java
+++ src/main/java/edu/cibertec/capitulo4/service/MatriculaService.java
@@ -5,11 +5,17 @@
  */
 package edu.cibertec.capitulo4.service;
 
+import edu.cibertec.capitulo4.dao.repository.AuditoriaRepositorio;
 import edu.cibertec.capitulo4.dao.repository.MatriculaRepositorio;
+import edu.cibertec.capitulo4.entity.AuditoriaEntity;
 import edu.cibertec.capitulo4.entity.MatriculaEntity;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 /**
  *
  * @author Adminfoco
@@ -18,12 +24,16 @@ import org.springframework.stereotype.Service;
 public class MatriculaService {
     @Autowired
     private MatriculaRepositorio matriculaRepositorio;
+    @Autowired
+    private AuditoriaRepositorio auditoriaRepositorio;
 
     public List<MatriculaEntity> listaTodas() {
         return matriculaRepositorio.findAll();
     }
-
+    @Transactional(propagation=Propagation.REQUIRED)
     public void grabarMatricula(MatriculaEntity me) {
         matriculaRepositorio.save(me);
+        AuditoriaEntity ae= new AuditoriaEntity(0,new Date(Calendar.getInstance().getTimeInMillis()),null,"Insertando Matricula "+me.getIdmatricula());
+        auditoriaRepositorio.save(ae);
     }
 }
